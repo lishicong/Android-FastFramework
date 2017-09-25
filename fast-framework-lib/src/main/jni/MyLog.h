98d4307d6f8ff7b72e980185d5dcd8b760068e56
lishicong <lishicong@baidu.com>
2017-09-25 14:46:12 +0800

    20170925

---
+++ fast-framework-lib/src/main/jni/MyLog.h
@@ -0,0 +1,19 @@
+//
+// Created by lishicong on 2017/9/20.
+//
+
+#ifndef _MYLOG_H_
+#define _MYLOG_H_
+
+#include <android/log.h>
+
+#define TAG "liscso"
+
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+
+#endif
--- fast-framework-lib/src/main/jni/com_fast_framework_ndk_AESHelper.cpp
+++ fast-framework-lib/src/main/jni/com_fast_framework_ndk_AESHelper.cpp
@@ -3,18 +3,15 @@
 //
 
 #include <jni.h> 
-#include <android/log.h> 
+#include "MyLog.h"
 #include "com_fast_framework_ndk_AESHelper.h"  
-
-#ifndef LOG_TAG
-#define LOG_TAG "System.out" 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#endif  
 
 const char *aes_key = "1234567890abcdef";
 
 JNIEXPORT jstring JNICALL Java_com_fast_framework_ndk_AESHelper_getAESKey(
         JNIEnv *env, jclass jclass1) {
+
+    LOGI("jni log :%s", " hello");
 
     return env->NewStringUTF(aes_key);
 }
